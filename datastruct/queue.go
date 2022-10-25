b45cf362c10b20db25106ba34641963e69a460c6
ZhengjunHUO <firelouiszj@hotmail.com>
2022-10-25 14:09:38 +0200

    add queue
    Signed-off-by: ZhengjunHUO <firelouiszj@hotmail.com>

---
+++ datastruct/queue.go
@@ -0,0 +1,79 @@
+package datastruct
+
+import (
+	"sync"
+)
+
+// Queue contains a slice of any type
+type Queue[V any] struct {
+	Elems []V
+	Lock  sync.RWMutex
+}
+
+// NewQueue creates a queue from a slice of any type
+func NewQueue[V any](slice []V) *Queue[V] {
+	q := &Queue[V]{}
+
+	for i := 0; i < len(slice); i++ {
+		q.Push(slice[i])
+	}
+
+	return q
+}
+
+// Push appends a new element of any type to the end
+func (q *Queue[V]) Push(elem V) {
+	q.Lock.Lock()
+	defer q.Lock.Unlock()
+
+	q.Elems = append(q.Elems, elem)
+}
+
+// Pop returns the queue's head and remove it from the queue
+func (q *Queue[V]) Pop() V {
+	var zeroVal V
+
+	if q.IsEmpty() {
+		return zeroVal
+	}
+
+	q.Lock.Lock()
+	defer q.Lock.Unlock()
+
+	defer func(){
+		q.Elems[0] = zeroVal
+		q.Elems = q.Elems[1:]
+	}()
+
+	return q.Elems[0]
+}
+
+// Peek returns the queue's head
+func (q *Queue[V]) Peek() V {
+	var zeroVal V
+
+	if q.IsEmpty() {
+		return zeroVal
+	}
+
+	q.Lock.RLock()
+	defer q.Lock.RUnlock()
+
+	return q.Elems[0]
+}
+
+// IsEmpty checks if the queue is empty
+func (q *Queue[V]) IsEmpty() bool {
+	q.Lock.RLock()
+	defer q.Lock.RUnlock()
+
+	return len(q.Elems) == 0
+}
+
+// Size returns the length of queue
+func (q *Queue[V]) Size() int {
+	q.Lock.RLock()
+	defer q.Lock.RUnlock()
+
+	return len(q.Elems)
+}
